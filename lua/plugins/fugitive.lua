a4e787ee131da18a99712da025960ea7eb2620bc
nakkeez <markus.rantaniva@gmail.com>
2025-11-02 17:22:56 +0200

    feat(fugitive): add keymaps for git commands

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,8 +1,13 @@
 return {
     "tpope/vim-fugitive",
     config = function ()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>gf", vim.cmd.Gdiff)
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Git: show status" })
+        vim.keymap.set("n", "<leader>gf", vim.cmd.Gdiffsplit, { desc = "Git: show difference" })
+        vim.keymap.set("n", "<leader>gq", "<cmd>Gwrite<cr>", { desc = "Git: add file" })
+        vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<cr>", { desc = "Git: commit changes" })
+        vim.keymap.set("n", "<leader>gpl", "<cmd>Git pull<cr>", { desc = "Git: pull changes" })
+        vim.keymap.set("n", "<leader>gpu", "<cmd>15 split|term git push<cr>", { desc = "Git: push changes" })
+        vim.keymap.set("v", "<leader>gb", ":Git blame<cr>", { desc = "Git: blame selected line" })
 
         local FugitiveGroup = vim.api.nvim_create_augroup("FugitiveGroup", {})
 
@@ -18,6 +23,9 @@ return {
                 local bufnr = vim.api.nvim_get_current_buf()
                 local opts = { buffer = bufnr, remap = false }
 
+                vim.keymap.set("n", "<leader>pl", function()
+                    vim.cmd.Git("pull")
+                end, opts)
                 vim.keymap.set("n", "<leader>pu", function()
                     vim.cmd.Git("push")
                 end, opts)
